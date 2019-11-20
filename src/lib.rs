02500b8efe6185ae0509b4454dad43720d3568ec
Jed Mao <jedmao@outlook.com>
2019-11-20 17:48:25 -0600

    Remove whitespace tokens

--- src/lib.rs
+++ src/lib.rs
@@ -25,127 +25,47 @@ pub struct INIParser;
 /// ```
 pub fn parse(contents: &str) -> Result<EditorConfigINIAST, Error<Rule>> {
 	match INIParser::parse(Rule::ini, contents) {
-		Ok(mut pairs) => {
-			let item = pairs.next().unwrap();
-			let mut current_section: Option<Section> = None;
-			let mut ast = EditorConfigINIAST {
-				version: "0.1.2".to_string(),
-				body: Vec::new(),
-			};
-
-			for line in item.into_inner() {
-				match line.as_rule() {
-					Rule::section => {
-						if current_section.is_some() {
-							ast.body.push(Item::Section(current_section.unwrap()));
-						}
-						current_section = Some(Section {
-							header: SectionHeader {
-								name: line.into_inner().next().unwrap().as_str().to_string(),
-								raws: Raws {
-									before: Some("".to_string()),
-									after: Some("".to_string()),
-									newline: Some("\n".to_string()),
-								},
-							},
-							body: Vec::new(),
-						});
-					}
-					Rule::pair => {
-						let mut inner_rules = line.into_inner();
-						let prop = Item::Pair(Pair {
-							name: inner_rules.next().unwrap().as_str().to_string(),
-							value: inner_rules.next().unwrap().as_str().to_string(),
-							raws: Raws {
-								before: Some("".to_string()),
-								after: Some("".to_string()),
-								newline: Some("\n".to_string()),
-							},
-						});
-						match &mut current_section {
-							Some(ref mut section) => {
-								section.body.push(prop);
-							}
-							None => {
-								ast.body.push(prop);
-							}
-						}
-					}
-					Rule::comment => {
-						let mut inner_rules = line.into_inner();
-						let comment = Item::Comment(Comment {
-							indicator: inner_rules.next().unwrap().as_str().to_string(),
-							value: inner_rules.next().unwrap().as_str().to_string(),
-							raws: Raws {
-								before: Some("".to_string()),
-								after: Some("".to_string()),
-								newline: Some(inner_rules.next().unwrap().as_str().to_string()),
-							},
-						});
-						match &mut current_section {
-							Some(ref mut section) => {
-								section.body.push(comment);
-							}
-							None => {
-								ast.body.push(comment);
-							}
-						}
-					}
-					Rule::blank_line => {
-						let mut inner_rules = line.into_inner();
-						let whitespace = inner_rules.next().unwrap().as_str().to_string();
-						let line = Item::Raws(Raws {
-							before: if whitespace == "" {
-								Some(whitespace)
-							} else {
-								None
-							},
-							after: None,
-							newline: Some(inner_rules.next().unwrap().as_str().to_string()),
-						});
-						match &mut current_section {
-							Some(ref mut section) => {
-								section.body.push(line);
-							}
-							None => {
-								ast.body.push(line);
-							}
-						}
-					}
-					Rule::ws => {
-						let line = Item::Raws(Raws {
-							before: Some(line.into_inner().next().unwrap().as_str().to_string()),
-							after: None,
-							newline: None,
-						});
-						match &mut current_section {
-							Some(ref mut section) => {
-								section.body.push(line);
-							}
-							None => {
-								ast.body.push(line);
-							}
-						}
-					}
-					Rule::EOI => {
-						if current_section.is_some() {
-							ast.body.push(Item::Section(current_section.unwrap()));
-							current_section = None;
-						}
-					}
-					_ => unreachable!(),
-				}
-			}
-
-			return Ok(ast);
+		Ok(pairs) => {
+			return Ok(EditorConfigINIAST {
+				version: "0.1.0".to_string(),
+				body: create_body(pairs),
+			});
 		}
 		Err(e) => Err(e),
 	}
 }
 
+fn create_body(pairs: pest::iterators::Pairs<'_, Rule>) -> Vec<Item> {
+	return pairs
+		.map(|p| match p.as_rule() {
+			Rule::section => {
+				let mut inner_rules = p.into_inner();
+				return Item::Section(Section {
+					name: inner_rules.next().unwrap().as_str().to_string(),
+					body: create_body(inner_rules),
+				});
+			}
+			Rule::pair => {
+				let mut inner_rules = p.into_inner();
+				return Item::Pair(Pair {
+					name: inner_rules.next().unwrap().as_str().to_string(),
+					value: inner_rules.next().unwrap().as_str().to_string(),
+				});
+			}
+			Rule::comment => {
+				let mut inner_rules = p.into_inner();
+				return Item::Comment(Comment {
+					indicator: inner_rules.next().unwrap().as_str().to_string(),
+					value: inner_rules.next().unwrap().as_str().to_string(),
+				});
+			}
+			_ => unreachable!(),
+		})
+		.collect();
+}
+
 #[derive(Debug)]
 enum Item {
-	Raws(Raws),
 	Comment(Comment),
 	Pair(Pair),
 	Section(Section),
@@ -159,33 +79,18 @@ pub struct EditorConfigINIAST {
 
 #[derive(Debug)]
 struct Section {
-	header: SectionHeader,
-	body: Vec<Item>,
-}
-
-#[derive(Debug)]
-struct SectionHeader {
 	name: String,
-	raws: Raws,
-}
-
-#[derive(Debug)]
-struct Raws {
-	before: Option<String>,
-	after: Option<String>,
-	newline: Option<String>,
+	body: Vec<Item>,
 }
 
 #[derive(Debug)]
 struct Pair {
 	name: String,
 	value: String,
-	raws: Raws,
 }
 
 #[derive(Debug)]
 struct Comment {
 	indicator: String,
 	value: String,
-	raws: Raws,
 }
