307c85c6477aada8b6a6c38c77532e668f79b346
Jed Mao <jedmao@outlook.com>
2019-11-22 16:00:26 -0600

    Add docs with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 //! # EditorConfig
 //!
-//! `editorconfig` is a collection of utilities that handle the parsing of
-//! EditorConfig-INI file contents into AST, which can then be modified,
-//! serialized and deserialized.
+//! A collection of utilities that handle the parsing of
+//! [EditorConfig-INI](https://editorconfig-specification.readthedocs.io/en/latest/#file-format)
+//! file contents into [AST](https://en.wikipedia.org/wiki/Abstract_syntax_tree),
+//! which can then be modified and/or serialized.
 
 extern crate pest;
 #[macro_use]
@@ -10,27 +11,29 @@ extern crate pest_derive;
 
 use pest::error::Error;
 use pest::Parser;
-use std::fmt;
+use std::{env, fmt};
 
 #[derive(Parser)]
 #[grammar = "ini.pest"]
-pub struct INIParser;
+struct INIParser;
 
-/// Parses EditorConfig-INI contents into AST.
+/// Parses [EditorConfig-INI](https://editorconfig-specification.readthedocs.io/en/latest/#file-format)
+/// contents into [AST](https://en.wikipedia.org/wiki/Abstract_syntax_tree).
 ///
 /// # Examples
 ///
 /// ```
+/// extern crate editorconfig;
+///
 /// let contents = "root=true";
 /// let ast = editorconfig::parse(contents);
+///
+/// assert_eq!(ast.unwrap().to_string(), contents);
 /// ```
 pub fn parse(contents: &str) -> Result<EditorConfigINIAST, Error<Rule>> {
 	match INIParser::parse(Rule::ini, contents) {
 		Ok(mut pairs) => {
-			return Ok(EditorConfigINIAST {
-				version: "0.1.0".to_string(),
-				body: create_body(pairs.next().unwrap()),
-			});
+			return Ok(EditorConfigINIAST::new(create_body(pairs.next().unwrap())));
 		}
 		Err(e) => Err(e),
 	}
@@ -72,10 +75,39 @@ fn create_body(pair: pest::iterators::Pair<'_, Rule>) -> Vec<Item> {
 		.collect();
 }
 
+/// The root [AST](https://en.wikipedia.org/wiki/Abstract_syntax_tree) node of
+/// a [parsed](fn.parse.html) INI file that conforms to the
+/// [EditorConfig INI file format](https://editorconfig-specification.readthedocs.io/en/latest/#file-format).
+///
+/// # Examples
+///
+/// ```
+/// extern crate editorconfig;
+///
+/// use editorconfig::{EditorConfigINIAST,Item,Pair};
+///
+/// let ast = EditorConfigINIAST::new(vec![
+///     Item::Pair(Pair {
+///         key: "root".to_string(),
+///         value: "true".to_string(),
+///     }),
+/// ]);
+///
+/// assert_eq!(ast.to_string(), "root=true\n");
+/// ```
 #[derive(Debug)]
 pub struct EditorConfigINIAST {
-	pub version: String,
+	version: String,
 	pub body: Vec<Item>,
+}
+
+impl EditorConfigINIAST {
+	fn new(body: Vec<Item>) -> Self {
+		EditorConfigINIAST {
+			version: env::var("version").unwrap(),
+			body,
+		}
+	}
 }
 
 impl fmt::Display for EditorConfigINIAST {
@@ -87,6 +119,8 @@ impl fmt::Display for EditorConfigINIAST {
 	}
 }
 
+/// Any number of items may be used within a prelude or
+/// [section](struct.section.html) body.
 #[derive(Debug)]
 pub enum Item {
 	Comment(Comment),
@@ -105,6 +139,23 @@ impl fmt::Display for Item {
 	}
 }
 
+/// Starts with either a `#` or `;` comment indicator on a new or blank line,
+/// followed by any characters until it reaches a newline or the end of input.
+///
+/// # Examples
+///
+/// ```
+/// extern crate editorconfig;
+///
+/// use editorconfig::Comment;
+///
+/// let comment = Comment {
+///     indicator: "#".to_string(),
+///     value: "hello",
+/// };
+///
+/// assert_eq!(comment.to_string(), "# hello\n");
+/// ```
 #[derive(Debug)]
 pub struct Comment {
 	indicator: String,
@@ -118,6 +169,22 @@ impl fmt::Display for Comment {
 	}
 }
 
+/// A key-value pair.
+///
+/// # Examples
+///
+/// ```
+/// extern crate editorconfig;
+///
+/// use editorconfig::Pair;
+///
+/// let pair = Pair {
+///     key: "left".to_string(),
+///     value: "right".to_string(),
+/// };
+///
+/// assert_eq!(pair.to_string(), "left=right\n");
+/// ```
 #[derive(Debug)]
 pub struct Pair {
 	key: String,
@@ -131,9 +198,37 @@ impl fmt::Display for Pair {
 	}
 }
 
+/// Starts with a header and ends just before another section begins.
+///
+/// # Examples
+///
+/// ```
+/// extern crate editorconfig;
+///
+/// use editorconfig::{Item,Section};
+///
+/// let section = Section {
+///     name: "header".to_string(),
+///     body: vec![
+///         Item::Comment {
+///             indicator: "#".to_string(),
+///             value: "beginning of section body".to_string(),
+///         },
+///         Item::Pair {
+///             key: "left".to_string(),
+///             value: "right".to_string(),
+///         },
+///     ],
+/// };
+///
+/// assert_eq!(section.to_string(), "[header]\nleft=right\n");
+/// ```
 #[derive(Debug)]
 pub struct Section {
+	/// The section header's name (i.e., the part between `[` and `]`).,
 	name: String,
+	/// Contains any number of items, which may only consist of
+	/// comments and pairs.
 	body: Vec<Item>,
 }
 
@@ -144,5 +239,21 @@ impl fmt::Display for Section {
 			item.fmt(formatter)?;
 		}
 		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn pair() {
+		let pair = Pair {
+			key: "left".to_string(),
+			value: "right".to_string(),
+		};
+
+		assert_eq!(pair.key, "left");
+		assert_eq!(pair.value, "right");
 	}
 }
